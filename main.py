578ba6b99eb49adb52121e29642d4ffbe14a0823
uplay1007 <118464329+uplay1007@users.noreply.github.com>
2023-03-05 20:08:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+from io import BytesIO
+import requests
+from PIL import Image
+from spn_find import sizes
+
+toponym_to_find = " ".join(sys.argv[1:])
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+response = requests.get(geocoder_api_server, params=geocoder_params)
+if not response:
+    pass
+json_response = response.json()
+delta, toponym_coodrinates, toponym_longitude, toponym_lattitude = sizes(json_response)
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
+}
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(map_api_server, params=map_params)
+Image.open(BytesIO(
+    response.content)).show()
